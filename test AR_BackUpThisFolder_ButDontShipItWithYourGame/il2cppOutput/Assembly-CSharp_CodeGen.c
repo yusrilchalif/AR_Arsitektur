0e2acff0ef60cb24157b5e2a3f7f5c351703a610
Yusril <yusrilchalif@gmail.com>
2023-06-22 11:22:25 +0700

    on progress spawn object, rotate and scale with gesture

---
+++ test AR_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,177 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CarBehaviour::Update()
+extern void CarBehaviour_Update_m68C7008EC9145CE19B744E926E584BC401739E5A (void);
+// 0x00000002 System.Void CarBehaviour::OnTriggerEnter(UnityEngine.Collider)
+extern void CarBehaviour_OnTriggerEnter_mA35F4D017DFA3595B36B27E19A2CCD19EF96B737 (void);
+// 0x00000003 System.Void CarBehaviour::.ctor()
+extern void CarBehaviour__ctor_m8B15D7D60CB46A05AF29CA03CDEB409B90D741D6 (void);
+// 0x00000004 System.Void CarManager::Update()
+extern void CarManager_Update_mA9352BCED72028603AD58436612CD9FD4142268F (void);
+// 0x00000005 System.Boolean CarManager::WasTapped()
+extern void CarManager_WasTapped_m21145A087BCD47727189878A742D7DA75A9257C1 (void);
+// 0x00000006 System.Void CarManager::.ctor()
+extern void CarManager__ctor_mDD4D293C5198DF4335D27D73B538F6A03E51B6B0 (void);
+// 0x00000007 System.Void DrivingSurfaceManager::LockPlane(UnityEngine.XR.ARFoundation.ARPlane)
+extern void DrivingSurfaceManager_LockPlane_mA82F67527379EA54AF2BDB20917FEAE05C8B9416 (void);
+// 0x00000008 System.Void DrivingSurfaceManager::Start()
+extern void DrivingSurfaceManager_Start_m0BE45E79D5B896D7C7C9FAABA7E562B8944ACC10 (void);
+// 0x00000009 System.Void DrivingSurfaceManager::Update()
+extern void DrivingSurfaceManager_Update_m0CBF41EB8F289994EBCEB74560B3F3F5DEF71BB8 (void);
+// 0x0000000A System.Void DrivingSurfaceManager::DisableNewPlanes(UnityEngine.XR.ARFoundation.ARPlanesChangedEventArgs)
+extern void DrivingSurfaceManager_DisableNewPlanes_m35E0F83185559253D77908F12F81CC166A938848 (void);
+// 0x0000000B System.Void DrivingSurfaceManager::.ctor()
+extern void DrivingSurfaceManager__ctor_mCE802B745EF658DD0D0E9A9475392FEF8B9DD72A (void);
+// 0x0000000C System.Void LightEstimation::Start()
+extern void LightEstimation_Start_mB96CEF52F77306D66DDD70E602B0893E7F222ED8 (void);
+// 0x0000000D System.Void LightEstimation::FrameReceived(UnityEngine.XR.ARFoundation.ARCameraFrameEventArgs)
+extern void LightEstimation_FrameReceived_m355E196D3C86A6B5A4D9F6DE4899A443CE923DE6 (void);
+// 0x0000000E System.Void LightEstimation::.ctor()
+extern void LightEstimation__ctor_m0CAD0C2BA0A694441BF3C946D481EA2CA7D6F7AF (void);
+// 0x0000000F System.Void PackageBehaviour::.ctor()
+extern void PackageBehaviour__ctor_m2390DEE3EC5CF4CAD769BFCB714043769E20E860 (void);
+// 0x00000010 UnityEngine.Vector3 PackageSpawner::RandomInTriangle(UnityEngine.Vector3,UnityEngine.Vector3)
+extern void PackageSpawner_RandomInTriangle_m76B7AC964B24F75F7C4F7853099C18092FD81C00 (void);
+// 0x00000011 UnityEngine.Vector3 PackageSpawner::FindRandomLocation(UnityEngine.XR.ARFoundation.ARPlane)
+extern void PackageSpawner_FindRandomLocation_m2091F6B07DD35BA9C3408AB2ED3103706AC7785B (void);
+// 0x00000012 System.Void PackageSpawner::SpawnPackage(UnityEngine.XR.ARFoundation.ARPlane)
+extern void PackageSpawner_SpawnPackage_m75B1E4939C5004656585C8B37635473FCEFBD30D (void);
+// 0x00000013 System.Void PackageSpawner::Update()
+extern void PackageSpawner_Update_m2B6F34EDB210BDE9B40E8F5688542B2F6D62F006 (void);
+// 0x00000014 System.Void PackageSpawner::.ctor()
+extern void PackageSpawner__ctor_m6A9A3ED5DA60B7F83889B7D9D6E7C586EB541A13 (void);
+// 0x00000015 System.Void ReticleBehaviour::Start()
+extern void ReticleBehaviour_Start_mA039052AF1E16911774EDEA0E413B90725FBBBC5 (void);
+// 0x00000016 System.Void ReticleBehaviour::Update()
+extern void ReticleBehaviour_Update_mF1D127313E6C912BE462516903B8854A24AB6925 (void);
+// 0x00000017 System.Void ReticleBehaviour::.ctor()
+extern void ReticleBehaviour__ctor_m2CBBD6D514BEBCAA06CB1544E1489C52DDCDA871 (void);
+// 0x00000018 System.Void Readme::.ctor()
+extern void Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6 (void);
+// 0x00000019 System.Void Readme/Section::.ctor()
+extern void Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B (void);
+// 0x0000001A System.Single UnityEngine.XR.ARFoundation.Samples.ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m2945B61548EEE9250BE692F8592A6176D8BA7A2A (void);
+// 0x0000001B System.Void UnityEngine.XR.ARFoundation.Samples.ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mF695F727A4B05DB7F8236445865092203FDE5639 (void);
+// 0x0000001C System.Void UnityEngine.XR.ARFoundation.Samples.ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_mAE8DB3C77C9418A1972DF38B0A9EB5EF978A41C1 (void);
+// 0x0000001D System.Void UnityEngine.XR.ARFoundation.Samples.ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_mFDE5CAE08F9A2A5FD0B0CADACB35C90E1A8C9990 (void);
+// 0x0000001E System.Void UnityEngine.XR.ARFoundation.Samples.ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_mEFB59B42F3EBD943D3DD11D07F489F13DFC715F9 (void);
+// 0x0000001F System.Void UnityEngine.XR.ARFoundation.Samples.ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_m7D4228920DB050CA200942FB953F4727DB19E2BA (void);
+// 0x00000020 System.Void UnityEngine.XR.ARFoundation.Samples.ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mBE84E3CEBD2C9EF15B5F498FF4CDF7E1EF5DD205 (void);
+// 0x00000021 System.Void UnityEngine.XR.ARFoundation.Samples.ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_m0553A1CAC9A932F42C3B0B8966F010964A2A3CAC (void);
+// 0x00000022 System.Void UnityEngine.XR.ARFoundation.Samples.ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_m48377EA229FE49C0705EFED67F49E7A82CB5BD1A (void);
+static Il2CppMethodPointer s_methodPointers[34] = 
+{
+	CarBehaviour_Update_m68C7008EC9145CE19B744E926E584BC401739E5A,
+	CarBehaviour_OnTriggerEnter_mA35F4D017DFA3595B36B27E19A2CCD19EF96B737,
+	CarBehaviour__ctor_m8B15D7D60CB46A05AF29CA03CDEB409B90D741D6,
+	CarManager_Update_mA9352BCED72028603AD58436612CD9FD4142268F,
+	CarManager_WasTapped_m21145A087BCD47727189878A742D7DA75A9257C1,
+	CarManager__ctor_mDD4D293C5198DF4335D27D73B538F6A03E51B6B0,
+	DrivingSurfaceManager_LockPlane_mA82F67527379EA54AF2BDB20917FEAE05C8B9416,
+	DrivingSurfaceManager_Start_m0BE45E79D5B896D7C7C9FAABA7E562B8944ACC10,
+	DrivingSurfaceManager_Update_m0CBF41EB8F289994EBCEB74560B3F3F5DEF71BB8,
+	DrivingSurfaceManager_DisableNewPlanes_m35E0F83185559253D77908F12F81CC166A938848,
+	DrivingSurfaceManager__ctor_mCE802B745EF658DD0D0E9A9475392FEF8B9DD72A,
+	LightEstimation_Start_mB96CEF52F77306D66DDD70E602B0893E7F222ED8,
+	LightEstimation_FrameReceived_m355E196D3C86A6B5A4D9F6DE4899A443CE923DE6,
+	LightEstimation__ctor_m0CAD0C2BA0A694441BF3C946D481EA2CA7D6F7AF,
+	PackageBehaviour__ctor_m2390DEE3EC5CF4CAD769BFCB714043769E20E860,
+	PackageSpawner_RandomInTriangle_m76B7AC964B24F75F7C4F7853099C18092FD81C00,
+	PackageSpawner_FindRandomLocation_m2091F6B07DD35BA9C3408AB2ED3103706AC7785B,
+	PackageSpawner_SpawnPackage_m75B1E4939C5004656585C8B37635473FCEFBD30D,
+	PackageSpawner_Update_m2B6F34EDB210BDE9B40E8F5688542B2F6D62F006,
+	PackageSpawner__ctor_m6A9A3ED5DA60B7F83889B7D9D6E7C586EB541A13,
+	ReticleBehaviour_Start_mA039052AF1E16911774EDEA0E413B90725FBBBC5,
+	ReticleBehaviour_Update_mF1D127313E6C912BE462516903B8854A24AB6925,
+	ReticleBehaviour__ctor_m2CBBD6D514BEBCAA06CB1544E1489C52DDCDA871,
+	Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6,
+	Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B,
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m2945B61548EEE9250BE692F8592A6176D8BA7A2A,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mF695F727A4B05DB7F8236445865092203FDE5639,
+	ARFeatheredPlaneMeshVisualizer_Awake_mAE8DB3C77C9418A1972DF38B0A9EB5EF978A41C1,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_mFDE5CAE08F9A2A5FD0B0CADACB35C90E1A8C9990,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_mEFB59B42F3EBD943D3DD11D07F489F13DFC715F9,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_m7D4228920DB050CA200942FB953F4727DB19E2BA,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mBE84E3CEBD2C9EF15B5F498FF4CDF7E1EF5DD205,
+	ARFeatheredPlaneMeshVisualizer__ctor_m0553A1CAC9A932F42C3B0B8966F010964A2A3CAC,
+	ARFeatheredPlaneMeshVisualizer__cctor_m48377EA229FE49C0705EFED67F49E7A82CB5BD1A,
+};
+static const int32_t s_InvokerIndices[34] = 
+{
+	6277,
+	5025,
+	6277,
+	6277,
+	6085,
+	6277,
+	5025,
+	6277,
+	6277,
+	4914,
+	6277,
+	6277,
+	4901,
+	6277,
+	6277,
+	9273,
+	10888,
+	5025,
+	6277,
+	6277,
+	6277,
+	6277,
+	6277,
+	6277,
+	6277,
+	6221,
+	5077,
+	6277,
+	6277,
+	6277,
+	4913,
+	5025,
+	6277,
+	11741,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	34,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
