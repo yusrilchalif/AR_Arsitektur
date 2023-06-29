ca9f34afd07b7fbb98dc2d50696008356d34d1c2
Yusril <yusrilchalif@gmail.com>
2023-06-29 17:41:02 +0700

    improve UI

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject panelViewer;
+    public bool isActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActiveDeactiveButton()
+    {
+        bool isActive = panelViewer.activeSelf;
+        if (isActive)
+        {
+            panelViewer.transform.DOScaleY(0f, 0.3f).SetEase(Ease.OutSine).OnComplete(() => panelViewer.SetActive(false));
+        }
+        else
+        {
+            panelViewer.SetActive(true);
+            panelViewer.transform.localScale = new Vector3(1f, 0f, 1f);
+            panelViewer.transform.DOScaleY(1f, 0.3f).SetEase(Ease.OutSine);
+        }
+            
+    }
+}
