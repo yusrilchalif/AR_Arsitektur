9d4e9e652d8d0fad8cabac1f15ab4b65df670a2d
Yusril <yusrilchalif@gmail.com>
2023-06-29 18:12:52 +0700

    fix rotate object

---
+++ Assets/Script/RotateObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPos = Input.GetTouch(0).deltaPosition;
+            transform.Rotate(0, touchDeltaPos.x * speed, 0);
+        }
+    }
+}
