54349b34d895250245f64e4838b5f82b4c95f6f6
Yusril <yusrilchalif@gmail.com>
2023-07-12 14:00:24 +0700

    update logic

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -14,7 +14,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        panelViewer.SetActive(false);
     }
 
     // Update is called once per frame
